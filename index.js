3f16f376e02fda22fba952c6cbd2612ac0dc427b
Amisha Kumari <amishakumari544@gmail.com>
2021-12-25 14:59:47 +0530

    :fire: pwa
    added PWA successfully

--- index.js
+++ index.js
@@ -130,3 +130,21 @@ let deferredPrompt = null;
 window.addEventListener("beforeinstallprompt", (e) => {
   deferredPrompt = e;
 });
+async function installPWA() {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then(({ outcome }) => {
+      if (outcome === "accepted") {
+        console.log("Your PWA has been installed");
+      } else {
+        console.log("User chose to not install your PWA");
+      }
+      deferredPrompt = null;
+    });
+  }
+}
+window.addEventListener("appinstalled", (evt) => {
+  localStorage.setItem("pwaInstalled", "yes");
+  pwaInstalled = true;
+  document.getElementById("installPWA").style.display = "none";
+});
